84679259ecfd9aa294a4b3839461fb133e84369c
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-06 08:04:04 -0500

    update deps

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-"${PYTHON}" -m pip install . \
+"${PYTHON}" -m pip install
+  -vv \
   --no-deps \
   --no-build-isolation \
-  --global-option="--with-libmemcached='${PREFIX}'"
+  --global-option \
+    "--with-libmemcached='${PREFIX}' --with-zlib='${PREFIX}' --with-sasl2='${PREFIX}'" \
+  .
