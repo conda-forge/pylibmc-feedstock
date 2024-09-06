366380280b77763cc2d40a2872b3e7e9cd3f8186
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-06 15:37:51 -0500

    remove custom build script

--- recipe/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-"${PYTHON}" -m pip install . \
-  -vv \
-  --no-deps \
-  --no-build-isolation \
-  --global-option \
-    "--with-libmemcached='${PREFIX}' --with-zlib='${PREFIX}'"
