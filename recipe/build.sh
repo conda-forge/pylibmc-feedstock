0a0cdfcf70fcc442eaf441ee630a11feac8caeb7
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-06 09:14:47 -0500

    remove sasl2

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-"${PYTHON}" -m pip install
+set -eux
+
+"${PYTHON}" -m pip install . \
   -vv \
   --no-deps \
   --no-build-isolation \
   --global-option \
-    "--with-libmemcached='${PREFIX}' --with-zlib='${PREFIX}' --with-sasl2='${PREFIX}'" \
-  .
+    "--with-libmemcached='${PREFIX}' --with-zlib='${PREFIX}'"
